3f7640ead36594d3d9314f10557de12eb52b4676
Szymon Ochnio <szymonochnio15@gmail.com>
2024-11-06 08:29:18 +0100

    Add workspace setup and first task solution

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aidevs3
-aidevs3 tasks and materials
+
+aidevs3 tasks solutions and other materials
+https://www.aidevs.pl/
+
+```bash
+python -m venv .venv
+
+./.venv/Scripts/Activate
+
+pip install -r requirements.txt
+```
---
+++ tasks/s00e01.py
@@ -0,0 +1,58 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from openai import OpenAI
+
+print('S00E01')
+
+
+api_key = os.getenv('OPENAI_API_KEY_FOR_AIDEVS')
+client = OpenAI(
+    api_key=api_key
+)
+
+
+def get_answer(content) -> str:
+    return client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Answer user question. If it is possible, answer with number and nothing more.",
+            },
+            {
+                "role": "user",
+                "content": content,
+            }
+        ],
+        model="gpt-4o"
+    ).choices[0].message.content
+
+
+# Download page content
+url = "https://xyz.ag3nts.org/"
+response = requests.get(url)
+response.raise_for_status()  # Raise an error for bad responses
+
+# Parse the page content and extract the text from the element with id=human-question
+soup = BeautifulSoup(response.content, 'html.parser')
+human_question_element = soup.find(id='human-question')
+content = human_question_element.get_text()
+
+print("\ncontent: ", content)
+
+# Pass this text to get_answer(content) function
+answer = get_answer(content)
+print("\nanswer: ", answer)
+
+# Send the returned answer with POST method as a form with values: username, password, and answer
+post_data = {
+    'username': 'tester',
+    'password': '574e112a',
+    'answer': answer
+}
+post_response = requests.post(url, data=post_data)
+post_response.raise_for_status()
+
+# Print formatted HTML of the response
+formatted_html = BeautifulSoup(post_response.content, 'html.parser').prettify()
+print("\nresponse:", formatted_html)
